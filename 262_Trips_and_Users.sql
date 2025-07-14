3576ebd2806564241dc05c85e18bf80bf1ab5114
Nidhan Jain <nidhanjain1406@gmail.com>
2025-07-14 13:12:48 +0530

    Cleanup unwanted files

--- 262_Trips_and_Users.sql
+++
@@ -1,7 +0,0 @@
-SELECT Request_at AS Day,
-ROUND(SUM(CASE WHEN Status != 'completed' THEN 1 ELSE 0 END)/COUNT(*),2) AS 'Cancellation Rate'
-FROM Trips t
-JOIN Users c ON t.Client_Id = c.Users_Id
-JOIN Users d ON t.Driver_Id = d.Users_Id
-WHERE c.Banned='No' AND d.Banned='No'
-GROUP BY Request_at;
